b64430865e7a10577d030fbfff34428d8acd6e31
jberdeski <130100312+jberdeski@users.noreply.github.com>
2024-04-24 11:41:04 -0700

    Update part2-question16.js

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -6,3 +6,10 @@ let statistics = {
   blackCars: 40,
   rareCars: 2
 };
+
+for (let prop in statistics) {
+  /* Here the ===  checks if the property starts with the letter 'r' and %2 !== 0 checks if the value is an odd number */
+  if (prop[0] === 'r' || statistics[prop] % 2 !== 0) {
+    console.log(statistics[prop]);
+  }
+}
